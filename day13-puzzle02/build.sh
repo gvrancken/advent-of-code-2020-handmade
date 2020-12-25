4b1ece8ed95d8f9eca8bd67afc51205758f84d58
Gilbert Vrancken <vrancken.gilbert@gmail.com>
2020-12-25 15:36:48 +0100

    Revert "day14p01"
    This reverts commit baeb64229a12c69ff3ce7948442fbb7c6d0cbf43.

--- day13-puzzle02/build.sh
+++ day13-puzzle02/build.sh
@@ -8,4 +8,4 @@ DISABLED_COMPILER_WARNINGS="
 -Wno-null-dereference
 "
 
-clang -g -O0 -Wall -Wshadow ${DISABLED_COMPILER_WARNINGS} -o osx_main osx_main.c
+clang -O3 -Wall -Wshadow ${DISABLED_COMPILER_WARNINGS} -o osx_main osx_main.c
